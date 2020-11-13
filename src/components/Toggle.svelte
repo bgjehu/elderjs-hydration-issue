14d26927a19d44732cef136138ada97fc1e17ed4
Jay Hu <jay@insiderx.com>
2020-11-13 11:38:12 -0600

    init

---
+++ src/components/Toggle.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { writable } from 'svelte/store';
+  const map = writable(new Map());
+  const open = key => {
+    map.update(map => {
+      map.set(key, {});
+      return map;
+    });
+  };
+  const close = key => {
+    map.update(map => {
+      map.delete(key);
+      return map;
+    });
+  };
+  const key = 'search';
+  const toggle = () => {
+    if ($map.has(key)) {
+      close(key);
+    } else {
+      open(key);
+    }
+  };
+</script>
+
+<button type="button" on:click={toggle}>toggle</button>
+{#if $map.has(key)}
+  <h1>Hello!</h1>
+{/if}
---
+++ src/routes/home/Home.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Toggle from '../../components/Toggle.svelte';
+  export let data, helpers;
+</script>
+
+<svelte:head>
+  <title>Elder.js Template: Home</title>
+</svelte:head>
+
+<Toggle hydrate-client={{}} />
